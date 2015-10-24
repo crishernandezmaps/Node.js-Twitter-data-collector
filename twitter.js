c482ed34197881112a793a2b3e0c2bd9ddcffb0b
gitvinit <vinitbadrike@yahoo.co.in>
2015-10-23 20:35:13 -0500

    Refactored code

--- twitter.js
+++ twitter.js
@@ -1,44 +1,45 @@
 (function() {
 
-var Twit = require('twit');
-var fs = require('fs');
-var T = new Twit({
-    consumer_key:''
-  , consumer_secret:''
-  , access_token:''
-  , access_token_secret:''
-});
+    var Twit = require('twit');
+    var fs = require('fs');
+    var T = new Twit({
+        consumer_key: '',
+        consumer_secret: '',
+        access_token: '',
+        access_token_secret: ''
+    });
 
-T.get('search/tweets', { q: '#trek', count: 100 }, function(err, data, response) {
-  writeToFile(data);
-})
+    T.get('search/tweets', {
+        q: '#trek',
+        count: 100
+    }, function(err, data, response) {
+        writeToFile(data);
+    })
 
-function writeToFile(data)
-{
-  var dataArray = [];
-  var fileData = {
-  "data":dataArray
-  };
+    function writeToFile(data) {
+        var dataArray = [];
+        var fileData = {
+            "data": dataArray
+        };
 
- for(var i=0;i<data.statuses.length;i++)
-  {
-    var element = {
-      "user":data.statuses[i].user.name,
-      "text":data.statuses[i].text,
-      "created_at":data.statuses[i].created_at
-    };
+        for (var i = 0; i < data.statuses.length; i++) {
+            var element = {
+                "user": data.statuses[i].user.name,
+                "text": data.statuses[i].text,
+                "created_at": data.statuses[i].created_at
+            };
 
-    dataArray.push(element);
-  }
+            dataArray.push(element);
+        }
 
-  fileData = JSON.stringify(fileData,null, 1);
+        fileData = JSON.stringify(fileData, null, 1);
 
-  fs.writeFile("data.json", fileData, function(err) {
-    if(err) {
-        return console.log(err);
+        fs.writeFile("data.json", fileData, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+
     }
-    console.log("The file was saved!");
-});
-
-}
 })();
